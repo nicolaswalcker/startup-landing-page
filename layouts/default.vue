230603ca0fdaa6b927425d94a7c40e5bdf3a129d
Nicolas <walcker_@outlook.com>
2023-07-19 15:11:21 -0300

    chore(general): adds resources, fonts, colors and images

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,15 +1,11 @@
 <template>
   <header
-    class="flex flex-col items-center bg-black py-2 border-b-[1px] border-[#00DC82]"
+    class="flex flex-col items-center bg-black py-2 border-b-[1px] border-[#3c584d]"
   >
     <h1 class="text-slate-200 text-xs">Default & Not Mobile Layout</h1>
     <p class="flex gap-2 text-white">
       <NuxtLink active-class="text-[#00DC82]" class="font-medium" to="/"
         >Home</NuxtLink
-      >
-
-      <NuxtLink active-class="text-[#00DC82]" class="font-medium" to="/about"
-        >About</NuxtLink
       >
     </p>
   </header>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,19 @@ import type { Config } from "tailwindcss";
 
 export default <Partial<Config>>{
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        graphik: ["Graphik", "sans-serif"],
+      },
+      colors: {
+        primary: "#4452FE",
+        secondary: "#02E4C0",
+        "primary-text": "#000000",
+        "secondary-text": "#5C5C5C",
+        "light-text": "#FFFFFF",
+        "bg-dark": "#181818",
+        "bg-light": "#FFFFFF",
+      },
+    },
   },
 };
